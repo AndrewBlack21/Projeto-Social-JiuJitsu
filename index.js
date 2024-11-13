866c818849049860d2af42851d6a7e0010628b69
AndrewBlack21 <andrew.moraes16@hotmail.com>
2024-11-12 23:25:42 -0300

    "Atualizando forma de ver perguntas e botoes"

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function toggleText() {
-    const moreText = document.querySelector('.more-text');
-    const btnText = document.querySelector('.txt-more');
 
-    if (moreText.style.display === 'none') {
-        moreText.style.display = 'inline';
-        btnText.textContent = 'Ver menos';
-    } else {
-        moreText.style.display = 'none';
-        btnText.textContent = 'Saiba mais!';
-    }
-}
 
 function toggleText2() {
     const moreText = document.querySelector('.more-text2');
